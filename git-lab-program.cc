e3f310d5029891a0ba456f07bccecffa703737ce
Rocco Pearce <rp529117@ohio.edu>
2020-01-21 10:56:58 -0500

    labprog

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Rocco Pearce
+ *        Date: 1-21-20
+ * 
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+ int main(int argc, char const *argv[]){
+     cout << "Hello Git!" << endl;
+     return 0;
+ }
